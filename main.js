5af05754239dc377febef903406914bdd1b06987
Asante Wiebers <67924684+AsanteWiebers@users.noreply.github.com>
2022-05-20 00:33:36 +0200

    js scrolll reveal added

---
+++ main.js
@@ -0,0 +1,21 @@
+ScrollReveal({
+    reset: true,
+    distance: '60px',
+    duration: 2500,
+    delay: 400
+});
+
+ScrollReveal().reveal('.img', {
+    delay: 100,
+    origin: 'bottom'
+});
+
+ScrollReveal().reveal('.collection-bg', {
+    delay:100,
+    origin: 'bottom'
+});
+ScrollReveal().reveal('.pairs-bg', {
+    delay: 100,
+    origin: 'bottom'
+});
+
